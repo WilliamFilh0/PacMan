b6649996b04c6092acdf96023a29a9aa82e3d25f
WilliamFilh0 <williamfilho8908@gmail.com>
2023-07-14 10:40:58 -0300

    setting the canvas boundary

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth 
 canvas.height = innerWidth 
+
+class Boundary{
+  constructor({position} ){
+    this.position = position
+    this.width = 40
+    this.height = 40
+  }
+}
