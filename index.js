e4efc6aedb40aa1b1ca79bb962478f57bc46cd34
WilliamFilh0 <williamfilho8908@gmail.com>
2023-07-18 00:45:15 -0300

    draw player

--- index.js
+++ index.js
@@ -28,7 +28,14 @@ class Player {
     this.velocity = velocity
     this.radius = 15
   }
-  
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'yellow'
+    c.fill()
+    c.closePath()
+
+  }
 }
 
 const map = [
@@ -45,7 +52,7 @@ const player = new Player({
     x: Boundary.width+ Boundary.width/2,
     y: Boundary.height + Boundary.height/2
   },
-  
+ 
 })
 
 map.forEach((row, i) => {
